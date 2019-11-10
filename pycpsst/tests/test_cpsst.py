68f955a9018bc33dbb6c14c273a982a53d9c64c5
Yuji Tamiya <tatamiya@YujinoMacBook-Pro.local>
2019-11-10 17:37:05 +0900

    pytest codes

---
+++ pycpsst/tests/test_cpsst.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from pycpsst import ChangePointSST
+
+
+def test_output_shape():
+    '''
+    Check whether the output of the 
+    ChangePointSST().score(X) method
+    is a float.
+    '''
+
+    signal = np.random.randn(200)
+    cp = ChangePointSST()
+
+    result = cp.score(signal)
+
+    assert isinstance(result, float)
